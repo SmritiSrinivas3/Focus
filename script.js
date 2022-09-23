01f6fc6f9db8de15e3dee6ee910ed9ccc2f06cce
SmritiSrinivas3 <smritisrinivasneelamana@gmail.com>
2022-09-23 12:17:47 +0530

    Made count into a fucnction

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-let pomodoro_time = 0
-let long_break_time = 0
-let short_break_time = 0
-let pomodoro_button_text = document.getElementById('pomodoro_button_text').innerText
-// assigning the user-input time 
-function assignTime() {
-    pomodoro_time = document.getElementById('sel-1').value
-    long_break_time = document.getElementById('sel-2').value
-    short_break_time = document.getElementById('sel-3').value
+// Declarations and definitions
+let pomodoro_time_in_min = document.getElementById('sel-1').value
+let long_break_time_in_min = document.getElementById('sel-2').value
+let short_break_time_in_min = document.getElementById('sel-3').value
+
+
+
+// counting function
+let time_passed_in_sec = 0
+
+function count(){ 
+   time_passed_in_sec++
+   console.log(time_passed_in_sec)
+  
+   
 }
 
-//start pomodoro counting
+function displayTimePomodoro(){
+    document.getElementById('pomodoro_button_text').innerText = `${Math.floor(time_passed_in_sec/60)}:${time_passed_in_sec%60}`
+}
+
+
 function startPomodoro() {
-    console.log('pomodoro started') // got this at the output
-    let num_of_min = 0 // to keep track of number of minutes passed
-    console.log(typeof (num_of_min)) // got it as 'number'
-    setInterval((num_of_min) => {
-        num_of_min++
-        console.log(num_of_min) //getting NaN at the console
-    }, 60000)
-
-
+    console.log('pomodoro started')
+    let time_passed_in_sec = 0
+    setInterval(()=>{count(), displayTimePomodoro()},1000)
 }
+
