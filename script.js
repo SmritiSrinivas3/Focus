be3a6728d9f96d6d09b60e8e6e4682f186739533
SmritiSrinivas3 <smritisrinivasneelamana@gmail.com>
2022-09-23 14:41:08 +0530

    Added all functionalities to pomodoro button except stop

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Declarations and definitions
-let pomodoro_time_in_min = document.getElementById('sel-1').value
-let long_break_time_in_min = document.getElementById('sel-2').value
-let short_break_time_in_min = document.getElementById('sel-3').value
+let pomodoro_time_in_min = 0
+let long_break_time_in_min = 0
+let short_break_time_in_min = 0
+document.getElementById('pomodoroButton').disabled = true
+document.getElementById('smallBreakButton').disabled = true
+document.getElementById('largeBreakButton').disabled = true
 
+
+
+function assignTime(){
+ pomodoro_time_in_min = document.getElementById('sel-1').value
+ long_break_time_in_min = document.getElementById('sel-2').value
+ short_break_time_in_min = document.getElementById('sel-3').value
+ document.getElementById('pomodoroButton').disabled = false
+ document.getElementById('smallBreakButton').disabled = false
+ document.getElementById('largeBreakButton').disabled = false
+}
 
 // counting function
 let time_passed_in_sec = 0
@@ -12,16 +25,27 @@ function count() {
     console.log(time_passed_in_sec)
 }
 
+
+// display text function
 function displayTimePomodoro(elem) {
      elem.innerText  = `${Math.floor(time_passed_in_sec / 60)}:${time_passed_in_sec % 60}`
+}
 
+// stop count fucntion
+function stopCount(elem){
+    elem.innerText = 'START'
 }
 
 
 function startPomodoro() {
     document.getElementById('pomodoroButton').disabled = true
+    document.getElementById('time-selection-button').disabled = true
     console.log('pomodoro started')
+    console.log(pomodoro_time_in_min)
     let time_passed_in_sec = 0
-    pomoInt = setInterval(() => { count(), displayTimePomodoro(pomodoro_button_text) }, 1000)
+    pomoInt = setInterval(() => { count(), displayTimePomodoro(pomodoro_button_text)}, 1000)
+    //setTimeout(() => { stopCount(pomodoro_button_text), 
+     //   document.getElementById('pomodoroButton').disabled = false
+   // }, pomodoro_time_in_min*60);
 }
 
