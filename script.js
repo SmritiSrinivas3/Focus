99619db11c0407f0621fe12b20494b10f3f95037
SmritiSrinivas3 <smritisrinivasneelamana@gmail.com>
2022-09-23 14:10:22 +0530

    Make display into a function

--- script.js
+++ script.js
@@ -4,25 +4,24 @@ let long_break_time_in_min = document.getElementById('sel-2').value
 let short_break_time_in_min = document.getElementById('sel-3').value
 
 
-
 // counting function
 let time_passed_in_sec = 0
 
-function count(){ 
-   time_passed_in_sec++
-   console.log(time_passed_in_sec)
-  
-   
+function count() {
+    time_passed_in_sec++
+    console.log(time_passed_in_sec)
 }
 
-function displayTimePomodoro(){
-    document.getElementById('pomodoro_button_text').innerText = `${Math.floor(time_passed_in_sec/60)}:${time_passed_in_sec%60}`
+function displayTimePomodoro(elem) {
+     elem.innerText  = `${Math.floor(time_passed_in_sec / 60)}:${time_passed_in_sec % 60}`
+
 }
 
 
 function startPomodoro() {
+    document.getElementById('pomodoroButton').disabled = true
     console.log('pomodoro started')
     let time_passed_in_sec = 0
-    setInterval(()=>{count(), displayTimePomodoro()},1000)
+    pomoInt = setInterval(() => { count(), displayTimePomodoro(pomodoro_button_text) }, 1000)
 }
 
