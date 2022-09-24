e676e5a43bb28b8103c785c198dfa47e9a75eb63
SmritiSrinivas3 <smritisrinivasneelamana@gmail.com>
2022-09-24 17:06:41 +0530

    Completed the project

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let pomodoro_time_in_min = 0
 let long_break_time_in_min = 0
 let short_break_time_in_min = 0
+let alarm = new Audio("alarm.mp3")
 // preventing the clicking of start buttons before setting time
 document.getElementById('pomodoroButton').disabled = true
 document.getElementById('smallBreakButton').disabled = true
@@ -10,8 +11,8 @@ document.getElementById('largeBreakButton').disabled = true
 
 function assignTime() {
     pomodoro_time_in_min = document.getElementById('sel-1').value
-    long_break_time_in_min = document.getElementById('sel-2').value
-    short_break_time_in_min = document.getElementById('sel-3').value
+    long_break_time_in_min = document.getElementById('sel-3').value
+    short_break_time_in_min = document.getElementById('sel-2').value
     // start buttons can be clicked now
     document.getElementById('pomodoroButton').disabled = false
     document.getElementById('smallBreakButton').disabled = false
@@ -23,8 +24,6 @@ let time_passed_in_sec = 0
 let time_passed_in_min = Math.floor(time_passed_in_sec / 60)
 function count() {
     time_passed_in_sec++
-    console.log(time_passed_in_sec)
-
 }
 
 
@@ -43,17 +42,72 @@ function stopCount(intVar, elem) {
     clearInterval(intVar)
     intVar = null
     elem.innerText = 'START'
+    alarm.play()
 }
+
 
 
 function startPomodoro() {
+    time_passed_in_sec = 0
+    time_passed_in_min = Math.floor(time_passed_in_sec / 60)
+    document.getElementById('name').innerText = 'FOCUS'
     document.getElementById('pomodoroButton').disabled = true  // to prevent multiple clicking
     document.getElementById('time-selection-button').disabled = true // to prevent multiple submission
+    document.getElementById('smallBreakButton').disabled = true // to prevent breaks
+    document.getElementById('largeBreakButton').disabled = true // to prevent breaks
     let pomoInt = setInterval(() => {
         count(), displayTime(pomodoro_button_text)
     }, 1000)
-
-
+    setTimeout(() => {
+        stopCount(pomoInt,pomodoro_button_text)
+       document.getElementById('name').innerText = 'WOAH!! YOU HAVE COMPLETED A SESSION'
+       document.getElementById('time-selection-button').disabled = false
+       document.getElementById('pomodoroButton').disabled = false
+       document.getElementById('smallBreakButton').disabled = false
+       document.getElementById('largeBreakButton').disabled = false
+    }, pomodoro_time_in_min*60*1000);
+    
 
 }
 
+function startShortBreak(){
+    time_passed_in_sec = 0
+    time_passed_in_min = Math.floor(time_passed_in_sec / 60)
+    document.getElementById('name').innerText = 'FOCUS'
+    document.getElementById('smallBreakButton').disabled = true  // to prevent multiple clicking
+    document.getElementById('time-selection-button').disabled = true // to prevent multiple submission
+    document.getElementById('pomodoroButton').disabled = true
+    document.getElementById('largeBreakButton').disabled = true
+    let shortBreakInt = setInterval(()=>{
+        count(), displayTime(short_break_button_text)
+    },1000)
+    setTimeout(()=>{
+        stopCount(shortBreakInt,short_break_button_text)
+        document.getElementById('name').innerText = 'BREAK TIME IS OVER'
+        document.getElementById('time-selection-button').disabled = false
+        document.getElementById('smallBreakButton').disabled = false
+        document.getElementById('pomodoroButton').disabled = false
+    document.getElementById('largeBreakButton').disabled = false
+    },short_break_time_in_min*60*1000)
+}
+
+function startLongBreak(){
+    time_passed_in_sec = 0
+    time_passed_in_min = Math.floor(time_passed_in_sec / 60)
+    document.getElementById('name').innerText = 'FOCUS'
+    document.getElementById('largeBreakButton').disabled = true  // to prevent multiple clicking
+    document.getElementById('time-selection-button').disabled = true // to prevent multiple submission
+    document.getElementById('smallBreakButton').disabled = true
+    document.getElementById('pomodoroButton').disabled = true
+    let longBreakInt = setInterval(()=>{
+        count(), displayTime(long_break_button_text)
+    },1000)
+   setTimeout(()=>{
+    stopCount(longBreakInt,long_break_button_text)
+    document.getElementById('name').innerText = 'BREAK TIME IS OVER'
+    document.getElementById('time-selection-button').disabled = false
+    document.getElementById('smallBreakButton').disabled = false
+    document.getElementById('pomodoroButton').disabled = false
+document.getElementById('largeBreakButton').disabled = false
+   },long_break_time_in_min*60*1000)
+}
